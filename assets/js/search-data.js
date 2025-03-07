3cc469925801fc50c98b159b2cb3b88df78419fa
yb23 <52319273+yb23@users.noreply.github.com>
2025-03-07 12:59:55 +0000

    Deploying to gh-pages from @ yb23/yb23.github.io@7972b14b0fcb5bc684480f95f287e352bd841702 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -417,16 +417,16 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+    },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-our-article-was-accepted-to-cvpr-2025-more-details-will-be-available-within-the-next-weeks",
+          title: 'Our article was accepted to CVPR 2025 ! (more details will be available...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
