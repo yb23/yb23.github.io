bb233aa1793430469f3371fab49e541a3fd86028
yb23 <52319273+yb23@users.noreply.github.com>
2025-03-17 15:23:16 +0000

    Deploying to gh-pages from @ yb23/yb23.github.io@d94e8b80cc7e9b50e5f45ada56f063244e48559d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,17 +414,7 @@ ninja.data = [{
           section: "News",},{id: "news-our-article-was-accepted-to-cvpr-2025-more-details-will-be-available-within-the-next-weeks",
           title: 'Our article was accepted to CVPR 2025 ! (more details will be available...',
           description: "",
-          section: "News",},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
+          section: "News",},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
